869e9244ceabf162bce24b6e06e14dfbe07f16b5
Benoit Jadinon <benoit@skipr.co>
2020-11-05 22:57:29 +0100

    cleaning up while learning ts

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # on-air
 
 node script that switches a smart light (Wemo) on/off based on computer camera status
+
+## TODO
+
+- handle the case where the previously connected light is disconnected, to avoid calling on it
+- error handling
+- learn typescript
+  
--- src/app.ts
+++ src/app.ts
@@ -3,35 +3,43 @@
 import isCameraOn from 'is-camera-on';
 import Wemo from 'wemo-client';
 import { interval, from, Observable, Observer, combineLatest } from 'rxjs';
-import { distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, map, startWith, switchMap, filter, tap } from 'rxjs/operators';
 
-const deviceSerialId = '221547K11000ED';
+const deviceSerialToFind = '221547K11000ED';
 
 const wemo = new Wemo();
 
-const device$ = new Observable(
-	(observer: Observer<WemoClient>) => {
-		wemo.discover((err:unknown , deviceInfo:IWemoDeviceInfo) => {
-			console.log(`Wemo discovered : ${deviceInfo.friendlyName} : ${deviceInfo.serialNumber}`)
-			if (deviceInfo.serialNumber == deviceSerialId) {
-				observer.next(wemo.client(deviceInfo));
+const wemoDiscoveredDevice$ = new Observable(
+	(observer: Observer<IWemoDeviceInfo>) => {
+		wemo.discover((err: unknown, deviceInfo: IWemoDeviceInfo) => {
+			if (err) {
+				observer.error(err);
+				return;
 			}
+			observer.next(deviceInfo);
 		});
-	} 
-).pipe(distinctUntilChanged());
+	}
+).pipe(
+	distinctUntilChanged((a, b) => a.serialNumber == b.serialNumber),
+	tap({ next: deviceInfo => console.log(`Wemo discovered : ${deviceInfo.friendlyName} : ${deviceInfo.serialNumber}`) }),
+)
 
-const cameraState$ = interval(1000)
+const wemoLight$ = wemoDiscoveredDevice$.pipe(
+	filter(device => device.serialNumber == deviceSerialToFind),
+	map((d) => wemo.client(d)),
+)
+
+const isCamOn$ = interval(1000)
 	.pipe(
 		startWith(0),
-		switchMap(() => 
-			from(isCameraOn())
-				.pipe(map((i:unknown) => i == true ? true : false))
+		switchMap(() =>
+			from(isCameraOn()).pipe(map(i => i as boolean))
 		),
 		distinctUntilChanged(),
-	);
+		tap({ next: isCamOn => console.debug(`webcam is ${isCamOn ? 'on' : 'off'}`) }),
+	)
 
-combineLatest([device$, cameraState$])
-	.subscribe(([wemoClient, isCamOn]) => { 
-		console.debug(isCamOn);
-		wemoClient.setBinaryState(isCamOn ? '1' : '0');
-	});
+combineLatest([wemoLight$, isCamOn$])
+	.subscribe(([wemoLight, isCamOn]) => {
+		wemoLight.setBinaryState((+!!isCamOn).toString() as "1" | "0");
+	})
